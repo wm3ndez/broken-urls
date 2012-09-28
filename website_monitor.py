66400d5657a823a6ae7ad0b4468e7ff50f0614a3
Williams Mendez <wmendez27@gmail.com>
2012-09-28 11:34:42 -0400

    Now BASE_URL must be an argument, plus other fixes

--- website_monitor.py
+++ website_monitor.py
@@ -3,14 +3,20 @@ import httplib
 import urllib2
 import time
 import sys
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('baseurl', help='Base Url')
+
+BASE_URL = parser.parse_args().baseurl
 bad_urls = []
-DOMAIN_BASE_URL = 'http://localhost:8000'
+all_urls = []
 exclude_urls = []
 
 
 def map_urls(url):
     try:
-        page = urllib2.urlopen(url) # maybe, I'm going off memory
+        page = urllib2.urlopen(url)
     except (urllib2.HTTPError,urllib2.URLError):
         bad_urls.append(url)
         return
@@ -21,19 +27,16 @@ def map_urls(url):
         try:
             if href.split('?')[0] in exclude_urls: continue
             if href.startswith('/'):
-                try:
-                    all_urls[href] += 1
-                except KeyError:
-                    all_urls[href] = 0
-                    print link.get('href')
-                    map_urls( '%s%s' % ( DOMAIN_BASE_URL, href))
+                if href in all_urls: continue
+                all_urls.append(href)
+                print link.get('href')
+                map_urls( '%s%s' % ( BASE_URL, href))
         except:
             pass
 
 
-all_urls = {}
-
-map_urls(DOMAIN_BASE_URL)
+#Let the fun begin
+map_urls(BASE_URL)
 print '*' * 15, 'BAD URLS', '*' * 10
 print ''
 print bad_urls
